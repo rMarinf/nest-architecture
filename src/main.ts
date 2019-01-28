4675bcb1e53489cdaedf0e2e6ae251ca39726eda
Roberto Marin <roberto.marin@intelygenz.com>
2019-01-28 12:41:27 +0100

    Add proxy to server

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as proxy from 'http-proxy-middleware';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, server);
+
+  // Add proxy
+  app.use([
+    '/oam*',
+    '/catalog/store/[0-9]+$',
+  ], proxy({ target: 'https://www.bershka.com/itxrest/2', changeOrigin: true , pathRewrite: {'^/api' : ''} }));
 
   // Add validation pipe
   app.useGlobalPipes(new ValidationPipe({
