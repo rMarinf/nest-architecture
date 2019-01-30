288ec38438768fa04590d111f0373925bb91864c
Roberto Marin <roberto.marin@intelygenz.com>
2019-01-30 17:19:09 +0100

    Add bunyan to show logs

--- src/cats/models/cat.model.ts
+++ src/cats/models/cat.model.ts
@@ -3,7 +3,13 @@ import * as uuidv4 from 'uuid/v4';
 
 export const CatSchema = new mongoose.Schema(
   {
-    hash: { type: String, trim: true, index: true, unique: true, default: uuidv4 },
+    hash: {
+      type: String,
+      trim: true,
+      index: true,
+      unique: true,
+      default: uuidv4,
+    },
     name: { type: String, trim: true, required: true },
     age: { type: Number, required: true },
     breed: { type: String, trim: true, required: true },
--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,16 @@ import * as paginate from 'express-paginate';
 import * as compression from 'compression';
 import * as helmet from 'helmet';
 import * as fs from 'fs';
+import * as bunyan from 'bunyan';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from './config/config.service';
 
+const log = bunyan.createLogger({ name: 'nest-architecture' });
+
 async function bootstrap() {
+  const config = new ConfigService(`${process.env.NODE_ENV}.env`);
   const key = fs.readFileSync('localhost-privkey.pem');
   const cert = fs.readFileSync('localhost-cert.pem');
   const options = {
@@ -24,10 +28,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, server, options);
 
   // Add morgan
-  // TODO: Llevar a configuración
-  app.use(
-    morgan(new ConfigService(`${process.env.NODE_ENV}.env`).get('MORGAN')),
-  );
+  app.use(morgan(config.get('MORGAN')));
 
   // Add compression
   app.use(compression());
@@ -59,6 +60,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(config.get('PORT'));
+
+  log.info(`Server listening in http://localhost:${config.get('PORT')}....`);
 }
 bootstrap();
